cc08ad4c0b294755997f1c9965bc1fe00e7a91fb
Hebert Brito <hbdev1211@gmail.com>
2023-07-15 14:51:39 -0300

    Pom.xml + .porperties

---
+++ src/main/java/com/hbdev/carrossb/CarrosSbApplication.java
@@ -0,0 +1,13 @@
+package com.hbdev.carrossb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrosSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrosSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hbdev/carrossb/CarrosSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbdev.carrossb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrosSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
