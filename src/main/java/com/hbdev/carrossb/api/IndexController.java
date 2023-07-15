aef25248ae3c495a191581d5e454400dd72ccf2a
Hebert Brito <hbdev1211@gmail.com>
2023-07-15 19:31:57 -0300

    GET - @RequestParam

--- src/main/java/com/hbdev/carrossb/api/IndexController.java
+++ src/main/java/com/hbdev/carrossb/api/IndexController.java
@@ -2,9 +2,12 @@ package com.hbdev.carrossb.api;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //-> Tranforma esse obj em um web service rest
@@ -16,16 +19,37 @@ public class IndexController {
 		return "GET SPRING BOOT!!!";
 	}
 	
-	@PostMapping
-	private String post() {
-		return "POST SPRING BOOT!!!";
-	}	
-	@PutMapping
-	private String put() {
-		return "PUT SPRING BOOT!!!";
-	}	
-	@DeleteMapping
-	private String delete() {
-		return "DELETE SPRING BOOT!!!";
-	}	
+//	@GetMapping("/login")//nao é boa pratica se passar senha no GET (sim POST)
+//	private String login(@PathVariable("login") String login,@PathVariable("senha") String senha) {
+//		return "Login:" + login + " Senha:" + senha;
+//	}
+	
+	@GetMapping("/login/{login}/{senha}")//nao é boa pratica se passar senha no GET (sim POST)
+	private String login(@PathVariable("login") String login,@PathVariable("senha") String senha) {
+		return "Login:" + login + " Senha:" + senha;
+	}
+	
+	@GetMapping("/carros/{id}")
+	public String getCarById(@PathVariable  Long id) {
+		return "Carro " + id;
+	}
+	
+	@GetMapping("/carros/tipo/{tipo}")
+	public String getCarByTipo(@PathVariable  String  tipo) {
+		return "Modelo do Carro: " + tipo;
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
 }
