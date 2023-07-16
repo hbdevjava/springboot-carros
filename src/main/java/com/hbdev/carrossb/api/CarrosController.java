ec01b7e1947e87d03d94e673388153e4a019c217
Hebert Brito <hbdev1211@gmail.com>
2023-07-16 18:56:48 -0300

    ResponseEntity

--- src/main/java/com/hbdev/carrossb/api/CarrosController.java
+++ src/main/java/com/hbdev/carrossb/api/CarrosController.java
@@ -5,6 +5,8 @@ package com.hbdev.carrossb.api;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,8 +29,9 @@ public class CarrosController {
 
 	
 	@GetMapping()
-	private Iterable<Carro> get() {
-		return carroService.getCarros();
+	private ResponseEntity<Iterable<Carro>> get() {
+		return ResponseEntity.ok(carroService.getCarros());
+		//return new ResponseEntity<>(carroService.getCarros(),HttpStatus.OK);
 		
 	}
 	
